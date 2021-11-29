0dbf1196c4c322f89f971011f9c2be3de907ad5b
kris.o <yajkakusta@gmail.com>
2021-11-29 20:03:10 +0500

    добавила readme - документация к коду. и изменила кодировку

---
+++ знакомство с языком программирования/ДвумерныеМассивыИРекурсия/RecursionAlgorithm/Program.cs
@@ -0,0 +1,24 @@
+﻿/* string[,]table = new string[2,3]; */
+/* int[,]matrix=new int[5,8];  */
+
+/* string[,] table = new string[2,5];
+// string,Empthy
+// table [0,0] table [0,1] table [0,2] table [0,3] table [0,4];
+// table [1,0] table [1,1] table [1,2] table [1,3] table [1,4]
+table[1,2]="слово";
+    for (int rows = 0; rows < 2; rows++)
+    {
+    for (int columns = 0; columns < 5; columns++)
+        {
+        System.Console.WriteLine($"-{table[rows, columns]}-");
+        }   
+    } */
+int[,]matrix=new int[3,4];
+    for (int rows = 0; rows < 3; rows++)
+    {
+        for (int columns = 0; columns < 4; columns++)
+        {
+            System.Console.WriteLine($"{matrix[rows, columns]} ");
+        }
+    Console.WriteLine();
+    }
