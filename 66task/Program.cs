e88cbb15b24f939954fccf3b5614a3dcf0e0648c
kris.o <yajkakusta@gmail.com>
2021-12-09 17:08:34 +0500

    создала отдельную папку для решения задачи от Сергея

--- 66task/Program.cs
+++ 66task/Program.cs
@@ -53,23 +53,38 @@ System.Console.WriteLine(NaturalNumber(5)); */
 System.Console.WriteLine(NaturalNumbers(20, 10));  */
 
 // При помощи тернарного оператора.
-/*  string NaturalNumbers(int m, int n)
+/* string NaturalNumbers(int m, int n)
 {
     if (m == n ) return  $"{n}" ;
     return (m>n) ? NaturalNumbers(m, n + 1) + " " + n: 
     NaturalNumbers(m, n - 1) + " " + n;
 }
-System.Console.WriteLine(NaturalNumbers(10, 20));   */
+System.Console.WriteLine(NaturalNumbers(10, 20));   */ 
+
 // 69. Найти сумму элементов от M до N, N и M заданы
 
-/* string SumOfNumbers(int m, int n)
+/*  int SumOfNumbers(int m, int n)
 {
-    int sum = m+(m+1);
-    return (m == n) ? $"{n}" : SumOfNumbers(m, sum) + " " + n;
-
+    if  (m == n) return n;
+    return (n>m) ? m + SumOfNumbers(m+1, n):
+    n + SumOfNumbers(m, n+1);
 }
- System.Console.WriteLine(SumOfNumbers(5, 7)); 
- */
+ System.Console.WriteLine(SumOfNumbers(3, 1));  */
+
+ // 2-ой способ. 
+/* int SumFromMtoN(int m, int n)
+{
+    if (n < m)
+    {
+        return m + SumFromMtoN(m - 1, n);
+    }    
+    if (m < n)
+    {
+        return n+ SumFromMtoN(m, n-1);
+    }
+    return n;
+}
+System.Console.WriteLine(SumFromMtoN(3, 1)); */ 
 
 // 70. Найти сумму цифр числа 
 
@@ -91,69 +106,49 @@ System.Console.WriteLine(NaturalNumbers(10, 20));   */
 System.Console.WriteLine(FunctionAckerman(3,4)); */
 
 // 72. Написать программу возведения числа А в целую стень B
-/// TO DO
-int DegreeNumber(int a)
+
+/* int b = 10;
+int a = 7;
+double DegreeNumber(int a, int b)
 {
-    int count = 2;
-    return ( a * a == 0||a * a == 1) ? a : DegreeNumber(a^count);
+    if (b == 0 ) return 1;
+    if (b == 1 ) return a;
+    if (b == 2) return a*a;
+    return a*DegreeNumber(a, b-1);
 }
- System.Console.WriteLine(DegreeNumber(6));
-/* 73. Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
-Первые два элемента последовательности задаются пользователем */
+for (int i = 0; i <= b; i++)
+    {
+    System.Console.Write($"{DegreeNumber(a, i)} ");
+    }  */
+// 73. Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+//Первые два элемента последовательности задаются пользователем 
 
 // Метод для ввода чисел от пользователя. 
-/*  string EnterToProgram()
+/* string EnterToProgram()
 {
+    int count1 = 5;
     System.Console.WriteLine("Введите первое число");
-    int N = Convert.ToInt32(Console.ReadLine());
+    int n1 = Convert.ToInt32(Console.ReadLine());
     System.Console.WriteLine("Введите второе число");
-    int M = Convert.ToInt32(Console.ReadLine());
-    string result = $"Вы ввели числа {N} и {M} ";
+    int m1 = Convert.ToInt32(Console.ReadLine());
+    string result = $"Вы ввели числа {n1} и {m1} ";
     return result;
-} */
-//System.Console.WriteLine(EnterToProgram());
-/// TO DO
-// Метод для вычисления сложения.
-/* double  Summary(int N, int M)
-{
-    int count = 5;
-    if (N == 0 || N == 1 && M == 0 || M == 1 ) return 1;
-    else return Summary (N - 1,  M-1) + Summary(N - 2,  M-2) ;
-    for (int i = 1; i < count; i++)
-        {
-        int j = i;
-        j++;
-        string result = $"{i} число = {Summary(N, M)}";
-        }
-}
-System.Console.WriteLine(Summary(2, 3));  */
-
-//1. Задача от Сергея.
-// пустая подпоследовательность - тоже является последовательностью
-//найти длину наибольшей общей последоловательности
-/* int[] array1 = { 1, 2, 3, 9, 5 };
-int[] аrray2 = { 1, 2, 9, 5, 4, 3 };
-int Length(int[] array1, int[] аrray2)
-{
-    int counter = 0;
-    int i = 0;
-    for (int j = 0; j <= array1.Length; j++)
+}  */
+/* int count1 = 10;
+System.Console.WriteLine("Введите первое число");
+int n1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int m1 = Convert.ToInt32(Console.ReadLine());
+string result = $"Вы ввели числа {n1} и {m1}";
+System.Console.WriteLine(result);
+int Summary(int n, int m, int count)
     {
-        if (array1[i] == аrray2[j])
-        {
-            counter++;
-            i++;
-        }
-        else 
-        {
-            j--;
-            i++;
-        }
+
+    if (count == 0) return n;
+    if (count == 1) return m;
+    return Summary(n, m, count - 1) + Summary(n, m, count - 2);
     }
-    return counter;
-}
-System.Console.WriteLine(Length(array1,аrray2)); */
-
-
-//правильный ответ 4
-
+for (int i = 0; i < count1; i++)
+    {
+    System.Console.Write($"{Summary(n1, m1, i)} ");
+    } */
---
+++ NOP_task/Program.cs
@@ -0,0 +1,25 @@
+﻿//1. Задача от Сергея.
+// пустая подпоследовательность - тоже является последовательностью
+//найти длину наибольшей общей последовательности
+/*  int[] array1 = { 1, 2, 3, 9, 5 };
+int[] аrray2 = { 1, 2, 9, 5, 4, 3 };
+int Length(int[] array1, int[] аrray2)
+{
+    int counter = 0;
+    int i = 0;
+    for (int j = 0; j <= array1.Length; j++)
+    {
+        if (array1[i] == аrray2[j])
+        {
+            counter++;
+            i++;
+        }
+        else 
+        {
+            j--;
+            i++;
+        }
+    }
+    return counter;
+}
+System.Console.WriteLine(Length(array1,аrray2));  */
